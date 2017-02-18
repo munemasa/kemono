604df4baaf4892a5defc5a60f02f210f5b4ada84
amano <amano@miku39.jp>
2017-02-18 20:39:38 +0900

    日本語以外を無効化

--- config/i18n.js
+++ config/i18n.js
@@ -21,33 +21,13 @@ var i18n = require('i18n');
 const initializeI18n = (app) => {
   i18n.configure({
     locales: [
-      'en', 'es', 'ja'
+      'ja'
     ],
     fallbacks: {
-      'en-US': 'en',
-      'es-AR': 'es',
-      'es-BO': 'es',
-      'es-CL': 'es',
-      'es-CO': 'es',
-      'es-CR': 'es',
-      'es-DO': 'es',
-      'es-EC': 'es',
-      'es-SV': 'es',
-      'es-GT': 'es',
-      'es-HN': 'es',
-      'es-MX': 'es',
-      'es-NI': 'es',
-      'es-PA': 'es',
-      'es-PY': 'es',
-      'es-PE': 'es',
-      'es-PR': 'es',
-      'es-ES': 'es',
-      'es-UY': 'es',
-      'es-VE': 'es',
       'ja-JP': 'ja'
     },
     directory: __dirname + '/../i18n',
-    defaultLocale: 'en'
+    defaultLocale: 'ja'
   });
 
   app.use(i18n.init);
